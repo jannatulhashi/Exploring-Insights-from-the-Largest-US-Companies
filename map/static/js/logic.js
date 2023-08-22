582f76d6ffbda26709ce13d24cafd69a61d8034d
TraceyGeneau <128385495+TraceyGeneau@users.noreply.github.com>
2023-08-21 22:04:38 -0400

    fixed map to include 2021

--- map/static/js/logic.js
+++ map/static/js/logic.js
@@ -12,12 +12,12 @@ function createMap(topCompanies) {
 
     // Create an overlayMaps object to hold the bikeStations layer.
     let overlayMaps = {
-        "Top Companies": topCompanies
-      };
+        "Top Companies 2021": topCompanies,
+    };
   // Create the map object with options.
   let map = L.map("map-id", {
-    center: [40.73, -74.0059],
-    zoom: 12,
+    center: [40.73, -94.0059],
+    zoom: 5,
     layers: [streetmap, topCompanies]
   });
 
@@ -29,28 +29,33 @@ function createMap(topCompanies) {
 }
 
 function createMarkers(response) {
+  // Pull the "stations" property from properties.company.
+  let companies = response.features;
+  
+  // Initialize an array to hold company markers.
+  let companyMarkers = [];
 
-    // Pull the "stations" property from properties.company.
-    let companies = properties.company;
+  // Sort the companies by Revenue in 2021 in descending order.
+  companies.sort((a, b) => parseInt(b.properties.Revenue_2021) - parseInt(a.properties.Revenue_2021));
+
+  let top20Companies =companies. slice(0,20);
+
+  // Loop through the top 20 companies.
+  for (let company of top20Companies) {
+    
+    // Create a marker for each company and bind a popup with the company's name and revenue.
+    let companyMarker = L.marker([company.geometry.coordinates[1], company.geometry.coordinates[0]])
+    .bindPopup("<h3>" + company.properties.Company + "</h3> City:" + company.properties.City + "</h3></br> State:" + company.properties.State + "</h3></br>Revenue(Millions) in 2021: " + company.properties.Revenue_2021 + "</h3></br>Profits(Millions) in 2021: " + company.properties.Profits_in_millions_2021 + "</h3>");
   
-    // Initialize an array to hold bike markers.
-    let companyMarkers = [];
   
-    // Loop through the stations array.
-    for (let index = 0; index < company.length; index++) {
-      let company = companies[index];
-  
-      // For each station, create a marker, and bind a popup with the station's name.
-      let companyMarker = L.marker([company.lat, company.lon])
-        .bindPopup("<h3>" + properties.Company + "<h3><h3>Revenue in 2021: " + properties.Revenue_2021 + "<h3><h3>Revenue in 2022: " + properties.Revenue_2022 + "</h3>");
-  
-      // Add the marker to the bikeMarkers array.
-      companyMarkers.push(companyMarker);
-    }
-  
-    // Create a layer group that's made from the bike markers array, and pass it to the createMap function.
-    createMap(L.layerGroup(companyMarkers));
+    // Add the marker to the companyMarkers array.
+    companyMarkers.push(companyMarker);
   }
 
-  // Perform an API call to the geojson API to get the station information. Call createMarkers when it completes.
+  // Create a layer group made from the company markers array and pass it to the createMap function.
+  createMap(L.layerGroup(companyMarkers));
+}
+
+
+// Perform an API call to the geojson API to get the station information. Call createMarkers when it completes.
 d3.json("http://127.0.0.1:5000/api/geojson").then(createMarkers);
